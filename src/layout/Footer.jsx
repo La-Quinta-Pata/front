6967c75b999584d2ded8281b62c798effcf10820
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-11-27 09:25:08 +0100

    feature:Added login button

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,19 +6,19 @@ function Footer() {
   return (
     <footer className="border-t border-lime-500 bg-linear-to-r from-gray-50 to-gray-100 mt-auto p-6 flex flex-col md:flex-row md:justify-around md:items-center gap-8">
       <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-all hover:scale-105 md:item-center">
-         <img alt='La Quinta Pata logo'  className="h-32 w-32 md:h-28 md:w-28 object-contain" src='/LaQuintaPataLogo.png'/>
+        <img alt='La Quinta Pata logo' className="h-32 w-32 md:h-28 md:w-28 object-contain" src='/LaQuintaPataLogo.png' />
       </Link>
-      <div className='flex justify-center md:justify-start gap-4 items-center'>
+      <section className='flex justify-center md:justify-start gap-4 items-center'>
         <Link className=" hover:opacity-80 transition-all hover:scale-105">Home</Link>
         <Link className=" hover:opacity-80 transition-all hover:scale-105">Acerca</Link>
         <Link className=" hover:opacity-80 transition-all hover:scale-105">Quines Somos</Link>
         <Link className=" hover:opacity-80 transition-all hover:scale-105">Catálogo</Link>
-      </div>
-      <div className='flex gap-4 items-center'>
-        <Link to="" className='hover:opacity-80 transition-all hover:scale-105'><SiFacebook size={36}/> </Link>
-        <Link to="" className='hover:opacity-80 transition-all hover:scale-105'><SiInstagram size={40}/></Link>
-        <Link to="" className='hover:opacity-80 transition-all hover:scale-105'><SiYoutube size={40}/> </Link>
-      </div>
+      </section>
+      <section className='flex gap-4 items-center'>
+        <Link to="" className='hover:opacity-80 transition-all hover:scale-105'><SiFacebook size={36} /> </Link>
+        <Link to="" className='hover:opacity-80 transition-all hover:scale-105'><SiInstagram size={40} /></Link>
+        <Link to="" className='hover:opacity-80 transition-all hover:scale-105'><SiYoutube size={40} /> </Link>
+      </section>
     </footer>
   )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,12 +5,12 @@ import Navbar from './Navbar';
 function Header() {
   return (
     <header className="border-b-4 border-lime-500 bg-white/95 backdrop-blur-sm shadow-sm">
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+      <section className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-all hover:scale-105">
           <img alt='La Quinta Pata logo'  className="h-16 w-16 object-contain" src='/LaQuintaPataLogo.png'/>
         </Link>
         <Navbar />
-      </div>
+      </section>
     </header>
   )
 }
