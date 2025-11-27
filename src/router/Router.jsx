59d473550aac3bc13ce2ddfaae14fde2173ab1f1
Mio <gusw.0246.lpsyg@gmail.com>
2025-11-27 09:29:58 +0100

    feature:pages & path created

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Layout from '../layout/Layout'
 import Home from '../pages/Home'
+import Catalog from '../pages/Catalog'
+import File from '../pages/File'
+import Contact from '../pages/Contact'
 
 function AppRouter() {
   return (
@@ -9,7 +12,10 @@ function AppRouter() {
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Home />}/>
-
+          <Route path='/catalogo' element={<Catalog />}/>
+          <Route path='/archivo/:id' element={<File />}/>
+          <Route path='/contacto' element={<Contact />}/>
+          <Route path="*" element={<div>Page not found</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
