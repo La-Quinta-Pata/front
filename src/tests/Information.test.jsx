af92f414b485fab41f13f2fbf72fabf1713af39c
Montc027 <lymomuca@gmail.com>
2025-12-01 10:13:53 +0100

    Tested test and modified Information.test.jsx file with the info in Information.jsx file

--- src/tests/Information.test.jsx
+++ src/tests/Information.test.jsx
@@ -4,8 +4,8 @@ import Information from "../components/sections/Information";
 describe("Information", () => {
   it("renderiza los textos correctamente", () => {
     render(<Information />);
-    const mainText = screen.getByText(/lorem ipsum/i);
-    const paragraph = screen.getByText(/Lorem, ipsum dolor sit amet/i);
+    const mainText = screen.getByText(/La Quinta Pata/i);
+    const paragraph = screen.getByText(/Es una Asociación Cultural generadora/i);
     expect(mainText).toBeInTheDocument();
     expect(paragraph).toBeInTheDocument();
   });
