d29eeeeaf6ba172d4a3efd1a8f2a280cb453a768
Montc027 <lymomuca@gmail.com>
2025-12-01 11:41:16 +0100

    Corrected test in information.test file

--- src/tests/Information.test.jsx
+++ src/tests/Information.test.jsx
@@ -4,9 +4,7 @@ import Information from "../components/sections/Information";
 describe("Information", () => {
   it("renderiza los textos correctamente", () => {
     render(<Information />);
-    const mainText = screen.getByText(/La Quinta Pata/i);
     const paragraph = screen.getByText(/Es una Asociación Cultural generadora/i);
-    expect(mainText).toBeInTheDocument();
     expect(paragraph).toBeInTheDocument();
   });
 });
