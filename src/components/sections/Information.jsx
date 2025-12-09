dad502560cb762e1e519cff520cc18c3187b62cd
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-12-09 12:01:31 +0100

    fix:added h1 to the new style

--- src/components/sections/Information.jsx
+++ src/components/sections/Information.jsx
@@ -6,19 +6,18 @@ export default function Information() {
     return (
 
         <section
-            className=" w-full 
-            text-center 
-            py-22 px-4 
+            className=" w-full
+            text-center
+            py-22 px-4
             flex flex-col items-center justify-center
-            bg-linear-to-b 
-            from-[rgba(253,240,213,0.5)] 
-            to-[#F77F00]
-          "
+            bg-linear-to-b
+            from-[rgba(253,240,213,0.5)]
+            to-[#F77F00"
             role="region"
             aria-labelledby="hero-heading"
         >
 
-           <Team />
+            <Team />
         </section>
 
 
