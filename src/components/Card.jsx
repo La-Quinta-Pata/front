a0758b51d7bbe60cbd31267a8924607f4913c520
surayac <surayacsm@gmail.com>
2025-12-09 09:23:28 +0100

    feature: add onclick to card to allow user to open video details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,51 +1,17 @@
-function Card({ image, author, country, description }) {
+function Card({ image, author, country, description, onClick }) {
   return (
-    <div className="
-      bg-white 
-      rounded-sm
-      border border-gray-200 
-      overflow-hidden
-      shadow-[#003049] shadow-md
-      hover:shadow-lg
-      hover:-translate-y-1 
-      transition-all 
-      duration-300 
-      flex flex-col
-      max-w-[330px]   
-      mx-auto
+    <article
+      className="cursor-pointer shadow-[#003049] shadow-md rounded-lg overflow-hidden hover:shadow-lg transition"
+      onClick={onClick}
+    >
+      <img src={image} alt={author} className="w-full h-48 object-cover p-4"/>
 
-    ">
-      
-      <div className="overflow-hidden">
-        <img 
-          src={image} 
-          alt={author} 
-          className="
-            w-full max-w-[300px]
-            h-48
-            p-3
-            object-cover 
-            transition-transform 
-            duration-300 
-            hover:scale-105
-          "
-        />
-      </div>
-
-      <div className="p-5 flex flex-col gap-2">
-        <h3 className="text-xl font-semibold tracking-tight text-gray-800">
-          {author}
-        </h3>
-
-        <p className="text-sm font-medium text-[#F77F00] uppercase tracking-wider">
-          {country}
-        </p>
-
-        <p className="text-gray-600 text-sm leading-relaxed line-clamp-2">
-          {description}
-        </p>
-      </div>
-    </div>
+      <footer className="p-3">
+        <h3 className="font-bold">{author}</h3>
+        <p className="text-sm text-gray-600">{country}</p>
+        <p className="text-xs mt-2 line-clamp-2">{description}</p>
+      </footer>
+    </article>
   );
 }
 
