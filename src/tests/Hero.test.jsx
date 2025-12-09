52fa389e7c86e398e3e8f98146fb1f067b0e39a8
Mio <gusw.0246.lpsyg@gmail.com>
2025-12-09 10:21:35 +0100

    test: update tests to support i18n

--- src/tests/Hero.test.jsx
+++ src/tests/Hero.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Hero from "../components/sections/Hero";
+import "../translation"
 
 describe("Hero", () => {
   it("renderiza el título correctamente", () => {
     render(<Hero />);
-    const heading = screen.getByRole("heading", { name: /La Quinta Pata/i });
+    const heading = screen.getByRole("heading", { name: /Memorias Migrantes/i });
     expect(heading).toBeInTheDocument();
   });
 });
