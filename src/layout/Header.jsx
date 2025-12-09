fa13572eee8e024ef8eee55c663d2dfddd4d72c9
surayac <surayacsm@gmail.com>
2025-12-09 09:42:44 +0100

    fix: edit logo from header and footer

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,11 +8,11 @@ function Header() {
     <header className="bg-[#FCBF49]/30 relative">
       <section className="w-full h-2 bg-[#FCBF49]/"></section>
 
-      <section className="container mx-auto px-2 flex items-center justify-between">
+      <section className="container mx-auto px-0 flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-all hover:scale-105">
           <img
             alt='Logo memorias migrantes'
-            className="h-20 w-auto sm:h-20 md:h-16 lg:h-20 object-contain drop-shadow-lg"
+            className="h-20 w-auto md:h-16 lg:h-22 object-contain drop-shadow-lg"
             src={logo}
           />
         </Link>
