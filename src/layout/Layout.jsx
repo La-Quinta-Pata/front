03950deaec88de082424fdb24f7d503abda52978
Mio <gusw.0246.lpsyg@gmail.com>
2025-12-12 12:50:33 +0100

    feature: collaboration section done, in layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router'
 import Footer from './Footer'
+import Collaboration from '../components/sections/Collaboration'
+import {collaboration} from "../data/collaborationData"
 
 function Layout() {
   return (
@@ -10,6 +12,7 @@ function Layout() {
       <main>
           <Outlet />
       </main>
+      <Collaboration collaboration={collaboration}/>
       <Footer />
     </section>
   )
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,21 +1,22 @@
 
-import AboutUsSection from "../components/sections/AboutUsSection";
-import aboutUsData from "../data/teamData";
-
-
+import AboutUsSection from "../components/sections/AboutUsSection"
+import {team} from "../data/teamData"
+import {academicCollaboration} from "../data/academicCollaborationData"
+import {webDesign} from "../data/webDesignData"
+import { useTranslation } from "react-i18next"
 export default function AboutUs() {
+  const {t} = useTranslation();
   return (
     <div className="w-full px-6 md:px-20 py-12">
-      <h1 className="text-center text-purple-950 text-4xl font-bold mb-10">Quiénes Somos</h1>
+      <h1 className="text-center text-purple-950 text-4xl font-bold mb-10">{t("aboutUs.whoAreWe")}</h1>
 
       <p className="max-w-3xl mx-auto text-center text-lg mb-16 text-purple-900">
-        La Quinta Pata es una Asociación Cultural que desde 2009 impulsa proyectos de pedagogía crítica a través del arte y la acción colectiva en torno a los temas de racismo, fronteras y género. Trabajamos con una mirada feminista, intercultural y decolonial, comprometidas con la Economía Social y Solidaria y la defensa de los Derechos Humanos.
-        Desde el corazón del Raval creamos espacios de resistencia para transformar experiencias migrantes en fuerza colectiva. Creemos en la cultura como herramienta política de transformación, memoria y justicia.
+        {t("aboutUs.description")}
       </p>
 
-      <AboutUsSection title="Equipo" items={aboutUsData.equipo} />
-      <AboutUsSection title="Colaboración Académica" items={aboutUsData.colaboracionAcademica} />
-      <AboutUsSection title="Diseño Web" items={aboutUsData.disenoWeb} />
+      <AboutUsSection title={t("aboutUs.team")} items={team} />
+      <AboutUsSection title={t("aboutUs.academicCollaboration")} items={academicCollaboration} />
+      <AboutUsSection title={t("aboutUs.webDesign")} items={webDesign} />
     </div>
   )
 }
